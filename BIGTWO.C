3609f51e44a10e5c0a326588b376f498cb33d762
Ashutosh1529 <chaudharyashu1529@gmail.com>
2024-09-22 23:13:49 +0530

    Add files via upload

---
+++ BIGTWO.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("Enter two number;-");
+scanf("%d%d",&a,&b);
+ if(a>b)
+  {
+   printf("a is greater");
+  }
+  else
+  {
+  printf("B is greater");
+  }
+
+ getch();
+}
---
+++ ODDEVEN.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("enter a number");
+scanf("%d",a);
+ if(a%2==0)
+   {
+    printf("number is even");
+   }
+ else
+     {
+     printf("number is odd");
+     }
+getch();
+}
